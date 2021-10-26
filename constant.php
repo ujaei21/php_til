d48f2bd99975cf2d4ade82876c2b9685cc270a96
ujaei <ujaei021@gmail.com>
2021-10-26 18:17:57 +0900

    php 2nd day

---
+++ constant.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <?php
+        print "현재 사용중인 php버전은?";
+        print PHP_VERSION;
+        
+        print "<br/> 이 파일의 이름은?";
+        print __FILE__;
+        
+        print "<br/> 수행 중에 있는 코드 라인은?";
+        print __LINE__;
+        ?>
+    </body>    
+</html>
+
+
